b7ce213a73cb218c2d44ab60c4aa38ce74c0246b
Matthew Gonzalez <matthewgonzalez7725@gmail.com>
2024-05-28 18:39:10 -0400

    package.json

--- server.js
+++ server.js
@@ -6,10 +6,31 @@ const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+const fs=require("fs")
+
+app.get("/api/notes",(req,res)=>{
+    fs.readFile("./db/db.json","utf8",(err,data)=>{
+        const newData=JSON.parse(data)
+        res.json(newData)
+    })
+})
+app.post("/api/notes",(req,res)=>{
+    
+})
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/notes.html"))
+})
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 
 
-app.get("*",(req,res))
 
-
-app.listen(PORT)
+app.listen(PORT,()=>{
+    console.log("App is listening at PORT:http://localhost:"+PORT)
+})
