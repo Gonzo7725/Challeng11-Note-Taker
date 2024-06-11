50342f51565af0ebb511166a3a8bfed55ce1d73c
Matthew Gonzalez <matthewgonzalez7725@gmail.com>
2024-06-11 19:55:37 -0400

    deploying

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Challeng11-Note-Taker
+
+## Description
+
+In this weeks chalenge we created a application that gives users a place to store notes to help them organize their thoughts and to keep track of tasks. It uses Express and implements an imitation database using a json file to save and retrieve data.
+
+## Installation 
+
+-Open in VS Code
+-Install Node.js v16 in termianl
+-Use npm init -y to initialize and create a package.json where project files will be stored
+-Use npm i to install dependencies
+-Use npm start to run the server 
+
+## Usage
+
+This app uses Express. For it to function properly, the server needs to be running in the background. To start the server, navigate to the directory of the application, install all dependencies (npm i), then type the command npm start (or node index.js). This should prompt "App listening at http://localhost:3001" in the terminal. Navigate to the front end of the application directly from the command line by holding the control key and clicking the link http://localhost:3001. Users can find any existing notes saved to the database or create new notes, which can be saved to the database.
+
+## Credits
+
+For help with understanding Node.js I used W3Schools.
+
+https://www.w3schools.com/nodejs/nodejs_intro.asp
+
+For help with JavaScript I referenced W3Schools.
+
+https://www.w3schools.com/js/
+
+## License
+
+MIT License
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const path=require("path")
+
 const PORT=process.env.PORT || 3001
 
 const app=express()
@@ -16,10 +17,46 @@ app.get("/api/notes",(req,res)=>{
         res.json(newData)
     })
 })
-app.post("/api/notes",(req,res)=>{
+
+app.post("/api/notes", (req, res) => {
     
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+       if (err) {
+           console.error(err);
+           return res.status(500).json({ error: 'Error reading notes data' })
+       }
+       const notes = JSON.parse(data )
+       const newNote = req.body
+       newNote.id = notes.length + 1 
+       notes.push(newNote);
+       fs.writeFile("./db/db.json", JSON.stringify(notes, null,"\t"), (err) => {
+           if (err) {
+               console.error(err);
+               return res.status(500).json({ error: 'Error writing new note' })
+           }
+           res.json(newNote)
+       })
+   })
 })
 
+app.delete("/api/notes/:id", (req, res) => {
+    const noteId = parseInt(req.params.id);
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Error Deleting Note' });
+        }
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId)
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ error: 'Internal Server Error' })
+            }
+            res.status(204).send()
+        })
+    })
+})
 app.get("/notes",(req,res)=>{
     res.sendFile(path.join(__dirname,"./public/notes.html"))
 })
